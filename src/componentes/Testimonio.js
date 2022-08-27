b0f990c92d4c8846998ba333fdd4f4041092725d
Ivan Carreño <carreaguileraivan@gmail.com>
2022-08-27 15:04:04 -0300

    Estructurando pagina de testimonio parte 1 freecodecamp

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+  function Testimonio (){
+    return (
+      <div className='contenedor-testimonio'>
+        <img className='imagen-testimonio' src={require('../imagenes/gringa.png')}
+       alt='Foto de Emma' />
+       <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
+        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
+        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos,
+         pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript,
+          así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades 
+          y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero
+           de software en Spotify."</p>
+       </div>
+      </div>
+    );
+}
+export default Testimonio
